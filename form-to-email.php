f2dbc60b6b8dde033966598f0942cefa40ba4695
Lina Sie <linahsie@gmail.com>
2018-07-31 21:04:25 -0700

    added description in index.html

--- form-to-email.php
+++ form-to-email.php
@@ -1,5 +1,22 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $message = $_POST['message'];
+
+// collect data
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+// validate
+if (empty($name) || empty($visitor_email)) {
+    echo "Name and email are mandatory!";
+    exit;
+}
+$email_from = "$name";
+$email_subject = "New Email submission";
+$email_body = "$message";
+
+$to = "solarchapterofficial@gmail.com";
+$headers = "From: $email_from \r\n";
+
+// send email
+mail($to, $email_subject, $email_body, $headers);
 ?>
